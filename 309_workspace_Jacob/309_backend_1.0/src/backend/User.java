72ac41f2963f6346fde6ae34837582898ec3fe2e
Stilwell <Stilwell@iastate.edu>
2017-09-06 18:01:52 -0500

    my workspace

---
+++ 309_workspace_Jacob/309_backend_1.0/src/backend/User.java
@@ -0,0 +1,21 @@
+package backend;
+
+public class User {
+
+String name;
+Double rateHR;
+Double hoursWorked;
+Double hoursPayed;
+String Project;
+String Address;
+String Boss;
+
+public void main(){
+//create user
+	
+}
+
+public void name(){
+	
+}
+}

575a933074fd2804a08e7a6d58a0a311b9451a93
devinj <devinj@iastate.edu>
2017-09-07 18:30:27 -0500

    added job class

---
+++ Database/Job.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class Job {
+   int jobID;
+   String jobname;
+   int jobtype;
+   String jobdesc;
+   int parentID;
+
+   public Job(int jobID, String jobname, int jobtype) {
+   		this.jobID = jobID;
+         this.jobname = jobname;
+         this.jobtype = jobtype;
+   }
+
+   public void setJobdesc(String e) {
+		jobdesc = e;
+   }
+
+
+   public void setparentID(String p) {
+		parentID = p;
+   }
+}

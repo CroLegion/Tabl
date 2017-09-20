e4d6fe997d71c4a13e1bcc7bc9b488c3a7c46d8e
devinj <devinj@iastate.edu>
2017-09-20 16:50:26 -0500

    added new user php file

--- Front_End/new_user.php
+++ Front_End/new_user.php
@@ -1,22 +1,41 @@
 <!DOCTYPE HTML>  
 <html>
 <head>
+ <style>
+ .error {color: #FF0000;}
+</style>
 </head>
 <body>  
 
 <?php
 // define variables and set to empty values
+$first_name_err = $last_name_err = $username_err = "";
 $first_name = $last_name = $username = $email = $phone = "";
+$success = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $first_name = test_input($_POST["first_name"]);
-  $last_name = test_input($_POST["last_name"]);
-  $username = test_input($_POST["username"]);
-  $email = test_input($_POST["email"]);
-  $phone = test_input($_POST["phone"]);
+  if (empty($_POST["first_name"])) {
+    $first_name_err = "First Name is required";
+  } else {
+    $first_name = user_input($_POST["first_name"]);
+  }
+
+  if (empty($_POST["last_name"])) {
+    $last_name_err = "Last Name is required";
+  } else {
+    $last_name = user_input($_POST["last_name"]);
+  }
+
+  if (empty($_POST["username"])) {
+    $username_err = "Username is required";
+  } else {
+    $username = user_input($_POST["username"]);
+  }
+  $email = user_input($_POST["email"]);
+  $phone = user_input($_POST["phone"]);
 }
 
-function test_input($data) {
+function user_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
@@ -34,13 +53,21 @@ function test_input($data) {
   <br><br>
   Email Address: <input type="text" name="email">
   <br><br>
-  Phone Number: <input type="text" name="pone">
+  Phone Number: <input type="text" name="phone">
   <br><br>
   <input type="submit" name="submit" value="Submit">  
 </form>
 
 <?php
-// send information to server
+// validate user information, send information to server and then return success if saved to SQL server
+if ($first_name != "Devin") {
+  $success = "New User created";
+} elseif ($first_name == "John") {
+  $success = "User info did not save";
+}
+
+
+echo $success;
 ?>
 
 </body>

924a444e6d24aec916481d9886d2aca910e0e5b7
Zgamer32 <iamagamer32@gmail.com>
2017-09-25 10:01:30 -0500

    Made some conversion work for tree display

--- Front_End/tree_display.php
+++ Front_End/tree_display.php
@@ -1,6 +1,19 @@
 <?php
 
+	//required imports
+	require 'database.php';
+	require 'util.php';
+	
+	//Define database parameters
+	$servername = "mysql.cs.iastate.edu";
+	$username = "dbu309amc2";
+	$password = "x1cbBr23";
+	$database = "db309amc2";
+	data_set($servername, $username, $password, $database);
 
+	$root = get_root_node();
+	$depth = get_tree_depth();
+	$children = get_children_in_order();
 
 /*
 <html>

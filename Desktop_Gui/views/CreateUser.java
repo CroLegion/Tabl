baacd28963e32c856e69ca49e72b81c0e939d511
devinj <devinj@iastate.edu>
2017-09-25 00:32:56 -0500

    Started the Desktop Gui

---
+++ Desktop_Gui/views/CreateUser.java
@@ -0,0 +1,180 @@
+package views;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import javax.swing.JTextField;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextPane;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Color;
+import javax.swing.UIManager;
+import java.awt.SystemColor;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class CreateUser extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JPasswordField passwordField;
+	private JTextPane txtpnPassword;
+	private JButton btnNewButton;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CreateUser frame = new CreateUser();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CreateUser() {
+		initComponents();
+		createEvents();
+		
+		
+	}
+
+	private void initComponents() {
+		setTitle("TABL");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(CreateUser.class.getResource("/resources/Logo.PNG")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 695, 488);
+		contentPane = new JPanel();
+		contentPane.setBackground(UIManager.getColor("Button.background"));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JTextPane txtpnFirstName = new JTextPane();
+		txtpnFirstName.setBackground(UIManager.getColor("Button.background"));
+		txtpnFirstName.setText("First Name:");
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		
+		JTextPane txtpnUsername = new JTextPane();
+		txtpnUsername.setText("Username:");
+		txtpnUsername.setBackground(SystemColor.menu);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		
+		JTextPane txtpnLastName = new JTextPane();
+		txtpnLastName.setText("Last Name:");
+		txtpnLastName.setBackground(SystemColor.menu);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		
+		JTextPane txtpnEmailAddress = new JTextPane();
+		txtpnEmailAddress.setText("Email Address:");
+		txtpnEmailAddress.setBackground(SystemColor.menu);
+		
+		JTextPane txtpnPhoneNumber = new JTextPane();
+		txtpnPhoneNumber.setText("Phone Number");
+		txtpnPhoneNumber.setBackground(SystemColor.menu);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		
+		txtpnPassword = new JTextPane();
+		txtpnPassword.setText("Password");
+		txtpnPassword.setBackground(SystemColor.menu);
+		
+		btnNewButton = new JButton("Create User");
+		
+		GroupLayout groupLayout = new GroupLayout(contentPane);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(69)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(txtpnFirstName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtpnLastName, GroupLayout.PREFERRED_SIZE, 61, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtpnUsername, GroupLayout.PREFERRED_SIZE, 61, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtpnEmailAddress, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtpnPhoneNumber, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtpnPassword, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(passwordField, Alignment.TRAILING)
+						.addComponent(textField_4, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 423, Short.MAX_VALUE)
+						.addComponent(textField_1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 423, Short.MAX_VALUE)
+						.addComponent(textField_2, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 423, Short.MAX_VALUE)
+						.addComponent(textField, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 423, Short.MAX_VALUE)
+						.addComponent(textField_3, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 423, Short.MAX_VALUE))
+					.addGap(37))
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+					.addGap(274)
+					.addComponent(btnNewButton)
+					.addContainerGap(306, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(57)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(txtpnFirstName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtpnLastName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(txtpnUsername, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtpnEmailAddress, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(txtpnPhoneNumber, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(41)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtpnPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(63)
+					.addComponent(btnNewButton)
+					.addContainerGap(63, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(groupLayout);
+		
+	}
+	private void createEvents() {
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+	}
+}

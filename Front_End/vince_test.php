7dd7d805bbeac8c73db7ca0c7551e55b999f99cb
vgwaters <vgwaters@iastate.edu>
2017-09-26 10:23:19 -0500

    did testing

---
+++ Front_End/vince_test.php
@@ -0,0 +1,18 @@
+<?php
+	//Import required PHP files
+	require 'database.php';
+	require 'util.php';
+
+	//Define database parameters
+	$servername = "mysql.cs.iastate.edu";
+	$username = "dbu309amc2";
+	$password = "x1cbBr23";
+	$database = "db309amc2";
+	data_set($servername, $username, $password, $database);
+	
+	$result = data_usersList();
+
+	echo $result;
+
+
+?>

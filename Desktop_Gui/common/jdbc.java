c8a58af7350e05e45fc17176d66c930973e40e55
devinj <devinj@iastate.edu>
2017-09-26 21:00:34 -0500

    gui changes

--- Desktop_Gui/common/jdbc.java
+++ Desktop_Gui/common/jdbc.java
@@ -1,43 +1,52 @@
 package common;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class jdbc {
 
+public static final String dbUrl = "jdbc:mysql://mysql.cs.iastate.edu:3306/db309amc2";
+public static final String user = "dbu309amc2";
+public static final String password = "x1cbBr23";
 
-public static void add_users(ArrayList<User> users) throws SQLException {
-	int usertype;
-	String username;
-	String firstname;
-	String lastname;
-	String email = "";
-	String phone = "";
-
+public static int get_user_id() throws SQLException {
+	Boolean duplicate_id_found = true;
+	int randomNum = 0;
+	while(duplicate_id_found) {
+		randomNum = ThreadLocalRandom.current().nextInt(0, Integer.MAX_VALUE);
+		// Connect to the database
+		Connection conn1;		
+		conn1 = DriverManager.getConnection(dbUrl, user, password);
+		System.out.println("*** Connected to the database ***");
+		String query = String.format("SELECT COUNT(userID) FROM db309amc2.users WHERE userID = %d", randomNum);
+		Statement stmt = null;
+		stmt = conn1.createStatement();
+		ResultSet rs = stmt.executeQuery(query);
+		System.out.println("userID tried: "+randomNum+rs.next());
+		stmt.close();
+		conn1.close();
+		
+	}
+	
+	return randomNum;
+	
+	
+}
+	
+	
+public static void add_user(int usertype, String username, String firstname, String lastname, String email, String phone, String passhash) throws SQLException {
+	int userID = get_user_id();
 	try {
 		// Connect to the database
-		Connection conn1;
-		String dbUrl = "jdbc:mysql://mysql.cs.iastate.edu:3306/db309amc2";
-		String user = "dbu309amc2";
-		String password = "x1cbBr23";
+		Connection conn1;		
 		conn1 = DriverManager.getConnection(dbUrl, user, password);
 		System.out.println("*** Connected to the database ***");
 
 		Statement statement = conn1.createStatement();
+		String sql = "INSERT INTO users " +
+           "VALUES ("+userID+","+usertype+",'"+username+"','"+firstname+"','"+lastname+"','"+email+"','"+phone+"','"+passhash+"');";
+		statement.executeUpdate(sql);
 		
-		for(int i = 0; i < users.size(); i++) {
-			int userID = users.get(i).userID;
-			usertype = users.get(i).usertype;
-			username = users.get(i).username;
-			firstname = users.get(i).firstname;
-			lastname = users.get(i).lastname;
-			email = users.get(i).email;
-			phone = users.get(i).phone;
-			String passhash = users.get(i).passhash;
-
-			String sql = "INSERT INTO users " +
-               "VALUES ("+userID+","+usertype+",'"+username+"','"+firstname+"','"+lastname+"','"+email+"','"+phone+"','"+passhash+"');";
-  			statement.executeUpdate(sql);
-		}
 
 		// Close all statements and connections
 		statement.close();
@@ -60,9 +69,6 @@ public static void add_project(ArrayList<Job> jobs) throws SQLException {
 	try {
 		// Connect to the database
 		Connection conn1;
-		String dbUrl = "jdbc:mysql://mysql.cs.iastate.edu:3306/db309amc2";
-		String user = "dbu309amc2";
-		String password = "x1cbBr23";
 		conn1 = DriverManager.getConnection(dbUrl, user, password);
 		System.out.println("*** Connected to the database ***");
 
@@ -97,9 +103,6 @@ public static ArrayList<User> get_users() {
 		try {
 		// Connect to the database
 		Connection conn1;
-		String dbUrl = "jdbc:mysql://mysql.cs.iastate.edu:3306/db309amc2";
-		String user = "dbu309amc2";
-		String password = "x1cbBr23";
 		conn1 = DriverManager.getConnection(dbUrl, user, password);
 		System.out.println("*** Connected to the database ***");
 
@@ -116,6 +119,8 @@ public static ArrayList<User> get_users() {
    			String email = rs.getString("email");
    			String phone = rs.getString("phone");
    			User u = new User(userID, usertype, username, firstname, lastname);
+   			u.setEmail(email);
+   			u.setPhone(phone);
    			users.add(u);
 		}
 		
@@ -141,9 +146,6 @@ public static void get_projects() throws SQLException {
 		try {
 		// Connect to the database
 		Connection conn1;
-		String dbUrl = "jdbc:mysql://mysql.cs.iastate.edu:3306/db309amc2";
-		String user = "dbu309amc2";
-		String password = "x1cbBr23";
 		conn1 = DriverManager.getConnection(dbUrl, user, password);
 		System.out.println("*** Connected to the database ***");
 

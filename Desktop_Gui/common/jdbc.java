15dbad3a6e8af224da31c8a5450d588fe479e907
devinj <devinj@iastate.edu>
2017-09-28 18:27:02 -0500

    fixing AdminManageUsers

--- Desktop_Gui/common/jdbc.java
+++ Desktop_Gui/common/jdbc.java
@@ -9,23 +9,25 @@ public static final String dbUrl = "jdbc:mysql://mysql.cs.iastate.edu:3306/db309
 public static final String user = "dbu309amc2";
 public static final String password = "x1cbBr23";
 
+
+/*Create a randon integer to be used as a user's userID between 1 and the max integer value.  
+ * Checks the data base to see if that Id is present, and then keeps trying until a new one is created.*/
 public static int get_user_id() throws SQLException {
-	Boolean duplicate_id_found = true;
 	int randomNum = 0;
-	while(duplicate_id_found) {
-		randomNum = ThreadLocalRandom.current().nextInt(0, Integer.MAX_VALUE);
+	
+	while(true) {
+		randomNum = ThreadLocalRandom.current().nextInt(1, Integer.MAX_VALUE);
 		// Connect to the database
 		Connection conn1;		
 		conn1 = DriverManager.getConnection(dbUrl, user, password);
 		System.out.println("*** Connected to the database ***");
-		String query = String.format("SELECT COUNT(userID) FROM db309amc2.users WHERE userID = %d", randomNum);
+		String query = String.format("SELECT userID FROM db309amc2.users WHERE userID = %d", randomNum);
 		Statement stmt = null;
 		stmt = conn1.createStatement();
 		ResultSet rs = stmt.executeQuery(query);
-		System.out.println("userID tried: "+randomNum+rs.next());
+		if (!rs.next()) {break;} //leave loop if new ID is found 
 		stmt.close();
 		conn1.close();
-		
 	}
 	
 	return randomNum;
@@ -44,7 +46,7 @@ public static void add_user(int usertype, String username, String firstname, Str
 
 		Statement statement = conn1.createStatement();
 		String sql = "INSERT INTO users " +
-           "VALUES ("+userID+","+usertype+",'"+username+"','"+firstname+"','"+lastname+"','"+email+"','"+phone+"','"+passhash+"');";
+           "VALUES ("+userID+",'"+username+"',"+usertype+",'"+firstname+"','"+lastname+"','"+email+"','"+phone+"','"+passhash+"');";
 		statement.executeUpdate(sql);
 		
 
@@ -123,22 +125,44 @@ public static ArrayList<User> get_users() {
    			u.setPhone(phone);
    			users.add(u);
 		}
-		
-		
-
 		// Close all statements and connections
 		stmt.close();
 		conn1.close();
 		
-		
-
 	} catch (SQLException e) {
 		System.out.println("SQLException: " + e.getMessage());
 		System.out.println("SQLState: " + e.getSQLState());
 		System.out.println("VendorError: " + e.getErrorCode());
 	}
 	return users;
+}
 
+public static User get_user(String username) {
+	User u = null;
+	try {
+	// Connect to the database
+	Connection conn1;
+	conn1 = DriverManager.getConnection(dbUrl, user, password);
+	System.out.println("*** Connected to the database ***");
+
+	String query = String.format("SELECT * FROM db309amc2.users WHERE username='%s'", username);
+	Statement stmt = null;
+	stmt = conn1.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	while (rs.next()) {
+		u = new User(rs.getInt("userID"), rs.getInt("userID"), rs.getString("username"), rs.getString("firstname"), rs.getString("lastname"));
+		u.setEmail(rs.getString("email"));
+		u.setPhone(rs.getString("phone"));
+	}
+	// Close all statements and connections
+	stmt.close();
+	conn1.close();
+	} catch (SQLException e) {
+		System.out.println("SQLException: " + e.getMessage());
+		System.out.println("SQLState: " + e.getSQLState());
+		System.out.println("VendorError: " + e.getErrorCode());
+	}
+	return u;
 }
 
 
--- Desktop_Gui/views/AdminManageUsers.java
+++ Desktop_Gui/views/AdminManageUsers.java
@@ -35,6 +35,11 @@ import javax.swing.JLabel;
 import com.jgoodies.forms.factories.DefaultComponentFactory;
 import javax.swing.UIManager;
 import javax.swing.JInternalFrame;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.JTextField;
+import javax.swing.JSeparator;
 
 public class AdminManageUsers extends JFrame {
 
@@ -42,6 +47,12 @@ public class AdminManageUsers extends JFrame {
 	private JButton btn_create_new_user;
 	private JButton btn_preferences;
 	private JButton btn_settings;
+	private JList<User[]> list;
+	private JTextField textFirstName;
+	private JTextField textLastName;
+	private JTextField textUsername;
+	private JTextField textEmail;
+	private JTextField textPhone;
 
 	/**
 	 * Launch the application.
@@ -76,7 +87,7 @@ public class AdminManageUsers extends JFrame {
 		setBackground(Color.RED);
 		setTitle("TABL");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1255, 815);
+		setBounds(100, 100, 938, 815);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -88,54 +99,220 @@ public class AdminManageUsers extends JFrame {
 		btn_preferences = new JButton("Preferences");
 		
 		
-		Panel panel = new Panel();
-		panel.setBackground(Color.LIGHT_GRAY);
+		Panel pnlUsers = new Panel();
+		pnlUsers.setBackground(Color.LIGHT_GRAY);
 		
 		btn_create_new_user = new JButton("Create New User");
 		
 		ArrayList<User> users = jdbc.get_users();
 		String[] users_names = get_users_names(users);
-		JList<User[]> list = new JList(users_names);
-		list.setBackground(UIManager.getColor("Button.background"));
-		list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-		list.setLayoutOrientation(JList.VERTICAL);
-		list.setVisibleRowCount(1);
 		
-
+		
+		JPanel pnlUserEditInfo = new JPanel();
+		pnlUserEditInfo.setBorder(new TitledBorder(null, "User Edit/Info", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		
+		JLabel lblUsers = DefaultComponentFactory.getInstance().createLabel("USERS");
+		pnlUsers.add(lblUsers);
+		lblUsers.setFont(new Font("Tahoma", Font.BOLD, 13));
+		
+		JScrollPane scrollPane = new JScrollPane();
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-						.addComponent(panel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(list, GroupLayout.DEFAULT_SIZE, 159, Short.MAX_VALUE))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btn_create_new_user, GroupLayout.PREFERRED_SIZE, 125, GroupLayout.PREFERRED_SIZE)
-					.addGap(713)
-					.addComponent(btn_preferences, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(btn_settings, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap())
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(pnlUsers, GroupLayout.PREFERRED_SIZE, 157, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btn_create_new_user, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
+							.addGap(329)
+							.addComponent(btn_preferences, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE)
+							.addGap(10)
+							.addComponent(btn_settings, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 155, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(pnlUserEditInfo, GroupLayout.PREFERRED_SIZE, 743, GroupLayout.PREFERRED_SIZE)))
+					.addGap(313))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(panel, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(list, GroupLayout.PREFERRED_SIZE, 276, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(pnlUsers, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 								.addComponent(btn_preferences)
 								.addComponent(btn_settings))
-							.addComponent(btn_create_new_user, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(452, Short.MAX_VALUE))
+							.addComponent(btn_create_new_user)))
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 720, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(1)
+							.addComponent(pnlUserEditInfo, GroupLayout.PREFERRED_SIZE, 733, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
 		);
 		
-		JLabel lblUsers = DefaultComponentFactory.getInstance().createLabel("USERS");
-		lblUsers.setFont(new Font("Tahoma", Font.BOLD, 13));
-		panel.add(lblUsers);
+		JLabel lblFullName = new JLabel("Full Name");
+		lblFullName.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		JLabel lblFirstName = new JLabel("First Name:");
+		lblFirstName.setFont(new Font("Tahoma", Font.BOLD, 14));
+		
+		JLabel lblLastName = new JLabel("Last Name:");
+		lblLastName.setFont(new Font("Tahoma", Font.BOLD, 14));
+		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setFont(new Font("Tahoma", Font.BOLD, 14));
+		
+		JLabel lblEmailAddress = new JLabel("Email Address:");
+		lblEmailAddress.setFont(new Font("Tahoma", Font.BOLD, 14));
+		
+		JLabel lblPhoneNumber = new JLabel("Phone Number:");
+		lblPhoneNumber.setFont(new Font("Tahoma", Font.BOLD, 14));
+		
+		textFirstName = new JTextField();
+		textFirstName.setColumns(10);
+		
+		textLastName = new JTextField();
+		textLastName.setColumns(10);
+		
+		textUsername = new JTextField();
+		textUsername.setColumns(10);
+		
+		textEmail = new JTextField();
+		textEmail.setColumns(10);
+		
+		textPhone = new JTextField();
+		textPhone.setColumns(10);
+		
+		JButton btnSaveChanges = new JButton("Save Changes");
+		
+		JSeparator separator = new JSeparator();
+		separator.setBackground(Color.BLACK);
+		separator.setForeground(Color.BLACK);
+		
+		JScrollPane scrlPaneAvailableQuals = new JScrollPane();
+		
+		JScrollPane scrlPaneAssignedQuals = new JScrollPane();
+		
+		JLabel lblAvailable = new JLabel("Available");
+		lblAvailable.setFont(new Font("Tahoma", Font.BOLD, 14));
+		
+		JLabel lblAssigned = new JLabel("Assigned");
+		lblAssigned.setFont(new Font("Tahoma", Font.BOLD, 14));
+		
+		JButton button = new JButton("->");
+		button.setFont(new Font("Tahoma", Font.BOLD, 16));
+		
+		JButton button_1 = new JButton("<-");
+		button_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		GroupLayout gl_pnlUserEditInfo = new GroupLayout(pnlUserEditInfo);
+		gl_pnlUserEditInfo.setHorizontalGroup(
+			gl_pnlUserEditInfo.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_pnlUserEditInfo.createSequentialGroup()
+					.addGroup(gl_pnlUserEditInfo.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_pnlUserEditInfo.createSequentialGroup()
+							.addGap(63)
+							.addGroup(gl_pnlUserEditInfo.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblFirstName)
+								.addComponent(lblLastName)
+								.addComponent(lblUsername)
+								.addComponent(lblEmailAddress)
+								.addComponent(lblPhoneNumber))
+							.addGap(80)
+							.addGroup(gl_pnlUserEditInfo.createParallelGroup(Alignment.LEADING)
+								.addComponent(textPhone, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE)
+								.addComponent(textEmail, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE)
+								.addComponent(textUsername, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE)
+								.addComponent(textLastName, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE)
+								.addComponent(textFirstName, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblFullName, GroupLayout.PREFERRED_SIZE, 127, GroupLayout.PREFERRED_SIZE)))
+						.addGroup(gl_pnlUserEditInfo.createSequentialGroup()
+							.addGap(123)
+							.addComponent(scrlPaneAvailableQuals, GroupLayout.PREFERRED_SIZE, 174, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addGroup(gl_pnlUserEditInfo.createParallelGroup(Alignment.LEADING)
+								.addComponent(button)
+								.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 53, GroupLayout.PREFERRED_SIZE))
+							.addGap(21)
+							.addComponent(scrlPaneAssignedQuals, GroupLayout.PREFERRED_SIZE, 174, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_pnlUserEditInfo.createSequentialGroup()
+							.addGap(24)
+							.addComponent(separator, GroupLayout.PREFERRED_SIZE, 699, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_pnlUserEditInfo.createSequentialGroup()
+							.addGap(278)
+							.addComponent(btnSaveChanges)))
+					.addContainerGap(8, Short.MAX_VALUE))
+				.addGroup(gl_pnlUserEditInfo.createSequentialGroup()
+					.addGap(179)
+					.addComponent(lblAvailable)
+					.addPreferredGap(ComponentPlacement.RELATED, 204, Short.MAX_VALUE)
+					.addComponent(lblAssigned, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE)
+					.addGap(202))
+		);
+		gl_pnlUserEditInfo.setVerticalGroup(
+			gl_pnlUserEditInfo.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_pnlUserEditInfo.createSequentialGroup()
+					.addComponent(lblFullName, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+					.addGap(30)
+					.addGroup(gl_pnlUserEditInfo.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_pnlUserEditInfo.createSequentialGroup()
+							.addComponent(textFirstName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(29)
+							.addComponent(textLastName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(30)
+							.addComponent(textUsername, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(31)
+							.addComponent(textEmail, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(33)
+							.addComponent(textPhone, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_pnlUserEditInfo.createSequentialGroup()
+							.addComponent(lblFirstName)
+							.addGap(29)
+							.addComponent(lblLastName)
+							.addGap(30)
+							.addComponent(lblUsername)
+							.addGap(31)
+							.addComponent(lblEmailAddress)
+							.addGap(33)
+							.addComponent(lblPhoneNumber)))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnSaveChanges)
+					.addGap(27)
+					.addComponent(separator, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGroup(gl_pnlUserEditInfo.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_pnlUserEditInfo.createSequentialGroup()
+							.addGap(13)
+							.addGroup(gl_pnlUserEditInfo.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblAvailable)
+								.addComponent(lblAssigned, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE))
+							.addGap(18)
+							.addGroup(gl_pnlUserEditInfo.createParallelGroup(Alignment.TRAILING)
+								.addComponent(scrlPaneAvailableQuals, GroupLayout.PREFERRED_SIZE, 271, GroupLayout.PREFERRED_SIZE)
+								.addComponent(scrlPaneAssignedQuals, GroupLayout.PREFERRED_SIZE, 271, GroupLayout.PREFERRED_SIZE)))
+						.addGroup(gl_pnlUserEditInfo.createSequentialGroup()
+							.addGap(127)
+							.addComponent(button)
+							.addGap(18)
+							.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)))
+					.addGap(30))
+		);
+		
+		JList listAssignedQuals = new JList();
+		scrlPaneAssignedQuals.setViewportView(listAssignedQuals);
+		
+		JList listAvailableQuals = new JList();
+		scrlPaneAvailableQuals.setViewportView(listAvailableQuals);
+		pnlUserEditInfo.setLayout(gl_pnlUserEditInfo);
+		list = new JList(users_names);
+		scrollPane.setViewportView(list);
+		list.setBackground(UIManager.getColor("Button.background"));
+		list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		list.setLayoutOrientation(JList.VERTICAL);
+		list.setVisibleRowCount(1);
 		contentPane.setLayout(gl_contentPane);
 		setForeground(Color.BLACK);
 		setIconImage(Toolkit.getDefaultToolkit().getImage(AdminManageUsers.class.getResource("/resources/Logo.PNG")));
@@ -157,15 +334,28 @@ public class AdminManageUsers extends JFrame {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
+		list.addListSelectionListener(new ListSelectionListener() {
+
+            @Override
+            public void valueChanged(ListSelectionEvent arg0) {
+                if (!arg0.getValueIsAdjusting()) {
+                  displayUserInfo((list.getSelectedValue().toString()));
+                }
+            }
+        });
 		
 	}
 	
 	private String[] get_users_names(ArrayList<User> data) {
 		String[] users = new String[data.size()];
 		for (int i = 0; i < data.size(); i++) {
-			System.out.println(data.get(i).get_firstname()+" "+data.get(i).get_lastname()+"["+data.get(i).get_username()+"]");
-			users[i] = data.get(i).get_firstname()+" "+data.get(i).get_lastname()+"["+data.get(i).get_username()+"]";
+			users[i] = data.get(i).get_firstname()+" "+data.get(i).get_lastname()+" ["+data.get(i).get_username()+"]";
 		}
 		return users;
+	}
+	
+	private void displayUserInfo(String username) {
+		User u = jdbc.get_user(username);
+		
 	}
 }

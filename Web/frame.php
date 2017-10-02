978f31a1deddf3ddb3e90c3b5838d7c5c2499395
Mason Wray <mewray@iastate.edu>
2017-10-02 17:32:00 -0500

    Added tracked files and re-merged.

---
+++ Web/frame.php
@@ -0,0 +1,22 @@
+<?php
+	$frame = <<< HTML
+	<html>
+		<head>
+			<title>Tabl</title>
+			<link rel='stylesheet' type='text/css href='styles.css'>
+		</head>
+		<body onload='initialize();'>
+			<div class='header'>
+				<h1>Page Header</h1>
+				<input class='topButton' type='button' value='Notifications' onclick='clickNotifications();'>
+				<input class='topButton' type='button' value='Settings' onclick='clickSettings();'>
+			</div>
+			<div class='page'>
+				<div class='navbar'> $navbar </div>
+				<div class='content' id='contentpane'> $content </div>
+				<div class='clear'><!-- --></div>
+			</div>
+		</body>
+	</html>
+HTML;
+?>

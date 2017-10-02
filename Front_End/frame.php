cc0c8c0862c20772c313ce43e8fccc5517e80177
mwray <mewray@iastate.edu>
2017-10-02 13:20:42 -0500

    Cleaned up a few junk/deprecated files in the web pages folder. Started 'messages.php. file.

--- Front_End/frame.php
+++ Front_End/frame.php
@@ -1,26 +1,31 @@
-<html>
-	<head>
-		<title>Tabl</title>
-		<script src='script.js'></script>
-		<link rel="stylesheet" type="text/css" href="styles.css">
-	</head>
-	<body onload='initialize();'>
-		<div class='header'>
-			<h1>Page Header</h1>
-			<input class='topButton' type='button' value='Notifications' onclick='clickNotifications();'>
-			<input class='topButton' type='button' value='Settings' onclick='clickSettings();'>
-		</div>
-		<div class='page'>
-			<div class='navbar'>
-				<input class='navbutton' type='button' value='Projects' onclick='clickProjects();'>
-				<div class='navlist' id='projectsButtons'></div>
-				<input class='navbutton' type='button' value='Messaging' onclick='clickMessages();'>
-				<div class='navlist' id='messagesButtons'></div>
+<?php
+
+	$frame = <<< HTML 
+	<html>
+		<head>
+			<title>Tabl</title>
+			<script src='script.js'></script>
+			<link rel="stylesheet" type="text/css" href="styles.css">
+		</head>
+		<body onload='initialize();'>
+			<div class='header'>
+				<h1>Page Header</h1>
+				<input class='topButton' type='button' value='Notifications' onclick='clickNotifications();'>
+				<input class='topButton' type='button' value='Settings' onclick='clickSettings();'>
 			</div>
-			<div class='content' id='contentpane'>
-				<p>Could not connect to server!</p>
+			<div class='page'>
+				<div class='navbar'>
+					<input class='navbutton' type='button' value='Projects' onclick='clickProjects();'>
+					<div class='navlist' id='projectsButtons'></div>
+					<input class='navbutton' type='button' value='Messaging' onclick='clickMessages();'>
+					<div class='navlist' id='messagesButtons'></div>
+				</div>
+				<div class='content' id='contentpane'>
+					<p>Could not connect to server!</p>
+				</div>
+				<div class='clear'><!-- --></div>
 			</div>
-			<div class='clear'><!-- --></div>
-		</div>
-	</body>
-</html>
+		</body>
+	</html>
+HTML;
+?>

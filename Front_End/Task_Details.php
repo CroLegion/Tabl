d1e211bb1a983aa6c1ad3093300456b60bd3d095
vgwaters <vgwaters@iastate.edu>
2017-10-02 12:24:24 -0500

    Made task detail page, still needs schema update

--- Front_End/Task_Details.php
+++ Front_End/Task_Details.php
@@ -14,7 +14,23 @@
 	data_set($servername, $username, $password, $database);
 	$taskID=2;
 	$results=get_task_by_id($taskID);
+	$results=$results->fetch_assoc();
+	echo "<h1>".$results["taskname"]."</h1>";
 
-	echo $results->fetch_assoc()["taskname"];
+	echo "<hr>";
 
+	echo "<h7>".$results["taskdesc"]."</h7>";
+
+	echo "<hr>";
+
+	echo "<h3>Task Details:</h3>";
+	echo "Deadline: TO BE ADDED<br/>";
+	echo "Created on: TO BE ADDED<br/>";
+	echo "Created by: TO BE ADDED<br/>";
+	echo "Task Status: TO BE ADDED<br/>";
+
+	echo "<hr>";
+
+	echo "<h3>Task Checklist:</h3>";
+	echo "Not currently in database, need to update schema";
 ?>

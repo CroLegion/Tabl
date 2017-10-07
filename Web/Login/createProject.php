4ded1c4d7e7079da752c16e592ddbb684cf3e211
vgwaters <vgwaters@iastate.edu>
2017-10-06 20:25:12 -0500

    I need to pay more attention to my branch

--- Web/Login/createProject.php
+++ Web/Login/createProject.php
@@ -1,6 +1,32 @@
 <?php
 	$title = "New Project";
-	$content = "This is the Create New Project Screen";
+
+
+	$content =  <<< HTML
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<title>Login</title>
+				<link rel='stylesheet' type='text/css' href='styles.css'>
+			</head>
+			<body>
+				<form action='index.php' method='post'>
+					<fieldset>
+						<legend>Please Authenticate</legend>
+						<label>Action:</label>
+						<br>
+						<input type='text' name='action'>
+						<br><br>
+						<label>Password:</label>
+						<br>
+						<input type='password' name='login_pass'>
+						<br><br>
+						<input type='submit' value='Create'>
+					</fieldset>
+				</form>
+			</body>
+		</html>
+HTML;
 
 	require 'navbar.php';
 	require 'frame.php';

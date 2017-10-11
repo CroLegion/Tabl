9bec7d98a5917efbbe4a1062512c88db5015becc
mwray <mewray@iastate.edu>
2017-10-11 01:17:22 -0500

    Made massive style changes to login screen.

--- Web/login.php
+++ Web/login.php
@@ -7,24 +7,35 @@
 		<html>
 			<head>
 				<title>Login</title>
+				<script src="processing.js"></script>
 				<link rel='stylesheet' type='text/css' href='styles.css'>
 			</head>
-			<body class='login'>
-				<form action='index.php' method='post'>
-					<fieldset>
-						<legend>Please Authenticate</legend>
+			<body>
+				<canvas data-processing-sources="background.pde" class='background'></canvas>
+				<div class='overlay'>
+					<h1 class='title'> Tabl </h1>
+					<form action='index.php' method='post' class='login'>
 						<p> $message </p>
-						<label>Username:</label>
+						<label class='loginlabel'>Username:</label>
 						<br>
-						<input type='text' name='login_user'>
+						<input type='text' name='login_user' class='loginfield'>
 						<br><br>
-						<label>Password:</label>
+						<label class='loginlabel'>Password:</label>
 						<br>
-						<input type='password' name='login_pass'>
+						<input type='password' name='login_pass' class='loginfield'>
 						<br><br>
-						<input type='submit' value='Submit'>
-					</fieldset>
-				</form>
+						<input type='submit' value='Sign In' class='loginbutton'>
+					</form>
+					<footer>
+						<div class="footerholder">
+   							<div class="footer">
+								<p> Scalable Workforce Management Solution </p>
+								<p> Devin Johnson &#183; Jacob Stilwell &#183; Vincent Waters &#183; Mason Wray</p>
+								<p><b> Tabl &#183; Version a0.2 &#183; 2017 </b></p>
+							</div>
+						</div>
+					</footer>
+				</div>
 			</body>
 		</html>
 HTML;

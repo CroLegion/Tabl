5be53e7ee03a6d21d40bde6307d8b16ea02e23f4
devinj <devinj@iastate.edu>
2017-10-14 11:36:35 -0500

    Minor changes

--- Database/JDBCPreparedStatement.java
+++ Database/JDBCPreparedStatement.java
@@ -1,7 +1,7 @@
 //STEP 1. Import required packages
 import java.sql.*;
 
-public class JDBCExample {
+public class JDBCPreparedStatement {
    // JDBC driver name and database URL
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
    static final String DB_URL = "jdbc:mysql://localhost/EMP";

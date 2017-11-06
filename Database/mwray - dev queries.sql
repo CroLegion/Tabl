6a3dd77ce79fac46b70098fbaba6d8bc5b0d25d7
mwray <mewray@iastate.edu>
2017-11-06 09:50:59 -0600

    Synchronized local changes with repo.

--- Database/mwray - dev queries.sql
+++ Database/mwray - dev queries.sql
@@ -3,3 +3,6 @@ SELECT * FROM users;
 UPDATE users
 SET usertype = 1, passhash = 'pass' 
 WHERE userID = 4352;
+
+SELECT * FROM conversations;
+INSERT INTO conversations (conversationName) VALUES ('Team Tabl');

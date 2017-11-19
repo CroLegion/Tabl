d2ab9a731d5cb123ae238e0a3382da58416c9b8b
Mason Wray <mewray@iastate.edu>
2017-11-19 00:28:06 -0600

    Started database files for modular access by desktop client.

---
+++ Web/Dynamic Web/dataserver.php
@@ -0,0 +1,7 @@
+<?php
+
+	//This file provides a 'connection' class for interacting with the MySQL database. This class should abstract database interactions to the level of 'getters' and 'setters'.
+	//By: Mason Wray - 11/19/2017
+
+	
+?>
